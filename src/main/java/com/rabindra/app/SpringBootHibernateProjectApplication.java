5786af98a2ea0be4b7be6c5948de1035eb9ac2e4
OakBindra <rabindradahal2015@gmail.com>
2022-02-04 15:38:14 +0545

    JUST GOT THE JOB DONE

--- src/main/java/com/rabindra/app/SpringBootHibernateProjectApplication.java
+++ src/main/java/com/rabindra/app/SpringBootHibernateProjectApplication.java
@@ -9,15 +9,16 @@ import org.springframework.boot.CommandLineRunner;
 //import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import com.rabindra.app.boot.Starter;
 //
-
+@EnableScheduling
 @SpringBootApplication
 public class SpringBootHibernateProjectApplication implements CommandLineRunner{
 
 	@Autowired
-	private static Starter starter;
+	private Starter starter;
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootHibernateProjectApplication.class, args);
 //		System.out.println(starter.getData());
@@ -27,8 +28,11 @@ public class SpringBootHibernateProjectApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 
-		System.out.println(starter.getData());
+//		System.out.println(starter.getData());
+		
+//		System.out.println(starter.getEmployees());
 //		System.out.println(applicationContext.getId());
+		System.out.println(starter.finalMethod());
 
 //		MyBean myBean = applicationContext.getBean(MyBean.class);
 //		System.out.println(myBean.getApplicationId());
--- src/main/java/com/rabindra/app/boot/Starter.java
+++ src/main/java/com/rabindra/app/boot/Starter.java
@@ -3,17 +3,18 @@ package com.rabindra.app.boot;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import com.rabindra.app.Repo.EmployeeRepo;
 import com.rabindra.app.entity.*;
 
-//@Component
+@Component
 public class Starter {
 
 	@Autowired
 	EmployeeRepo empRepo;
-	
+	int counttemp = 0;
 	public int getData() {
 		int count = empRepo.getCount();
 		
@@ -31,15 +32,53 @@ public class Starter {
 //		System.out.println("count"+getData());
 //	}
 	
+//	@Scheduled(fixedDelayString = "180000", initialDelay = 10000)
+	@Scheduled(cron="* * * * * *")
 	public void finalMethod() {
-		int counttemp = 0;
-		System.out.println("total user"+getData());
-		if(counttemp != getData()) {
-			counttemp++;
+		
+		int count = getData();
+		if(count != counttemp) {
+			System.out.println("Fetching all users data");
 			System.out.println("All the users:"+getEmployees());
 		}else {
-			System.out.println("Data cant be fetched.");
+			System.out.println("Count is Same so not Fetching");
 		}
 	}
-
 }
+
+//public class Starter {
+//
+//	@Autowired
+//	EmployeeRepo empRepo;
+//	
+//	public int getData() {
+//		int count = empRepo.getCount();
+//		
+//		return count;
+//	}
+//	
+//	//aba users fetch gara import gara ta milena 
+//	public List<Employee> getEmployees(){
+//		List<Employee> user = empRepo.getAll();
+//		return user;
+//	}
+//	// yeuta method banayera main class vitra call garthyeeau hola tyo method lai 
+////	public void getResult() {
+////		System.out.println("count"+getData());
+////		System.out.println("count"+getData());
+////	}
+//	
+//	public void finalMethod() {
+//		int counttemp = 0;
+//		System.out.println("total user"+getData());
+//		if(counttemp != getData()) {
+//			counttemp++;
+//			System.out.println("All the users:"+getEmployees());
+//		}else {
+//			System.out.println("Data cant be fetched.");
+//		}
+//	}
+//
+//}
+	
+	
